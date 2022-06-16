db73442af45c949e55b30956a61814b79b9d9dbd
Florian <florian@inflex.at>
2022-06-16 11:46:25 +0200

    Dependencies update process implemented

--- YoutubeToolsApp/Form1.Designer.cs
+++ YoutubeToolsApp/Form1.Designer.cs
@@ -38,9 +38,21 @@
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnDownload = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pbFfmpeg = new System.Windows.Forms.ProgressBar();
+            this.llFfmpeg = new System.Windows.Forms.LinkLabel();
+            this.lblStatusFfmpeg = new System.Windows.Forms.Label();
+            this.btnUpdateFfmpeg = new System.Windows.Forms.Button();
+            this.llYdl = new System.Windows.Forms.LinkLabel();
+            this.lblStatusYdl = new System.Windows.Forms.Label();
+            this.btnUpdateYdl = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pbYdl = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // webView
@@ -53,7 +65,7 @@
             this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
             this.webView.Location = new System.Drawing.Point(12, 65);
             this.webView.Name = "webView";
-            this.webView.Size = new System.Drawing.Size(938, 267);
+            this.webView.Size = new System.Drawing.Size(924, 262);
             this.webView.TabIndex = 0;
             this.webView.ZoomFactor = 1D;
             // 
@@ -63,13 +75,13 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbSearch.Location = new System.Drawing.Point(37, 36);
             this.tbSearch.Name = "tbSearch";
-            this.tbSearch.Size = new System.Drawing.Size(832, 23);
+            this.tbSearch.Size = new System.Drawing.Size(818, 23);
             this.tbSearch.TabIndex = 1;
             // 
             // btnSearch
             // 
             this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSearch.Location = new System.Drawing.Point(875, 36);
+            this.btnSearch.Location = new System.Drawing.Point(861, 36);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(75, 23);
             this.btnSearch.TabIndex = 2;
@@ -79,9 +91,9 @@
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 515);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 510);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(962, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(948, 22);
             this.statusStrip1.TabIndex = 3;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -92,7 +104,7 @@
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(962, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(948, 24);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -122,9 +134,9 @@
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.btnDownload);
-            this.groupBox1.Location = new System.Drawing.Point(12, 338);
+            this.groupBox1.Location = new System.Drawing.Point(12, 333);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(938, 174);
+            this.groupBox1.Size = new System.Drawing.Size(605, 174);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Download Control";
@@ -133,16 +145,124 @@
             // 
             this.btnDownload.Location = new System.Drawing.Point(6, 22);
             this.btnDownload.Name = "btnDownload";
-            this.btnDownload.Size = new System.Drawing.Size(131, 85);
+            this.btnDownload.Size = new System.Drawing.Size(131, 60);
             this.btnDownload.TabIndex = 0;
             this.btnDownload.Text = "Download";
             this.btnDownload.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.pbFfmpeg);
+            this.groupBox2.Controls.Add(this.llFfmpeg);
+            this.groupBox2.Controls.Add(this.lblStatusFfmpeg);
+            this.groupBox2.Controls.Add(this.btnUpdateFfmpeg);
+            this.groupBox2.Location = new System.Drawing.Point(623, 333);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(313, 84);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "FFMPEG";
+            // 
+            // pbFfmpeg
+            // 
+            this.pbFfmpeg.Location = new System.Drawing.Point(6, 51);
+            this.pbFfmpeg.Name = "pbFfmpeg";
+            this.pbFfmpeg.Size = new System.Drawing.Size(301, 23);
+            this.pbFfmpeg.TabIndex = 5;
+            // 
+            // llFfmpeg
+            // 
+            this.llFfmpeg.AutoSize = true;
+            this.llFfmpeg.Location = new System.Drawing.Point(81, 0);
+            this.llFfmpeg.Name = "llFfmpeg";
+            this.llFfmpeg.Size = new System.Drawing.Size(226, 15);
+            this.llFfmpeg.TabIndex = 4;
+            this.llFfmpeg.TabStop = true;
+            this.llFfmpeg.Text = "https://github.com/BtbN/FFmpeg-Builds";
+            this.llFfmpeg.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llFfmpeg_LinkClicked);
+            // 
+            // lblStatusFfmpeg
+            // 
+            this.lblStatusFfmpeg.AutoSize = true;
+            this.lblStatusFfmpeg.Location = new System.Drawing.Point(157, 26);
+            this.lblStatusFfmpeg.Name = "lblStatusFfmpeg";
+            this.lblStatusFfmpeg.Size = new System.Drawing.Size(39, 15);
+            this.lblStatusFfmpeg.TabIndex = 1;
+            this.lblStatusFfmpeg.Text = "Ready";
+            // 
+            // btnUpdateFfmpeg
+            // 
+            this.btnUpdateFfmpeg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdateFfmpeg.Location = new System.Drawing.Point(6, 22);
+            this.btnUpdateFfmpeg.Name = "btnUpdateFfmpeg";
+            this.btnUpdateFfmpeg.Size = new System.Drawing.Size(145, 23);
+            this.btnUpdateFfmpeg.TabIndex = 0;
+            this.btnUpdateFfmpeg.Text = "Update";
+            this.btnUpdateFfmpeg.UseVisualStyleBackColor = true;
+            this.btnUpdateFfmpeg.Click += new System.EventHandler(this.btnUpdateFfmpeg_Click);
+            // 
+            // llYdl
+            // 
+            this.llYdl.AutoSize = true;
+            this.llYdl.Location = new System.Drawing.Point(81, 0);
+            this.llYdl.Name = "llYdl";
+            this.llYdl.Size = new System.Drawing.Size(221, 15);
+            this.llYdl.TabIndex = 5;
+            this.llYdl.TabStop = true;
+            this.llYdl.Text = "https://github.com/ytdl-org/youtube-dl";
+            this.llYdl.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llYdl_LinkClicked);
+            // 
+            // lblStatusYdl
+            // 
+            this.lblStatusYdl.AutoSize = true;
+            this.lblStatusYdl.Location = new System.Drawing.Point(157, 26);
+            this.lblStatusYdl.Name = "lblStatusYdl";
+            this.lblStatusYdl.Size = new System.Drawing.Size(39, 15);
+            this.lblStatusYdl.TabIndex = 3;
+            this.lblStatusYdl.Text = "Ready";
+            // 
+            // btnUpdateYdl
+            // 
+            this.btnUpdateYdl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdateYdl.Location = new System.Drawing.Point(6, 22);
+            this.btnUpdateYdl.Name = "btnUpdateYdl";
+            this.btnUpdateYdl.Size = new System.Drawing.Size(145, 23);
+            this.btnUpdateYdl.TabIndex = 2;
+            this.btnUpdateYdl.Text = "Update";
+            this.btnUpdateYdl.UseVisualStyleBackColor = true;
+            this.btnUpdateYdl.Click += new System.EventHandler(this.btnUpdateYdl_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.pbYdl);
+            this.groupBox3.Controls.Add(this.llYdl);
+            this.groupBox3.Controls.Add(this.lblStatusYdl);
+            this.groupBox3.Controls.Add(this.btnUpdateYdl);
+            this.groupBox3.Location = new System.Drawing.Point(623, 423);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(313, 84);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "youtube-dl";
+            // 
+            // pbYdl
+            // 
+            this.pbYdl.Location = new System.Drawing.Point(6, 51);
+            this.pbYdl.Name = "pbYdl";
+            this.pbYdl.Size = new System.Drawing.Size(301, 23);
+            this.pbYdl.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(962, 537);
+            this.ClientSize = new System.Drawing.Size(948, 532);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.statusStrip1);
@@ -158,6 +278,10 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,5 +299,15 @@
         private Label label1;
         private GroupBox groupBox1;
         private Button btnDownload;
+        private GroupBox groupBox2;
+        private Button btnUpdateFfmpeg;
+        private LinkLabel llYdl;
+        private LinkLabel llFfmpeg;
+        private Label lblStatusYdl;
+        private Button btnUpdateYdl;
+        private Label lblStatusFfmpeg;
+        private GroupBox groupBox3;
+        private ProgressBar pbFfmpeg;
+        private ProgressBar pbYdl;
     }
 }
--- YoutubeToolsApp/Form1.cs
+++ YoutubeToolsApp/Form1.cs
@@ -1,4 +1,7 @@
+using System.IO.Compression;
+using System.Net;
 using System.Web;
+using YoutubeToolsApp.Data;
 
 namespace YoutubeToolsApp
 {
@@ -21,6 +24,100 @@ namespace YoutubeToolsApp
         private async void Form1_Load(object sender, EventArgs e)
         {
             await webView.EnsureCoreWebView2Async();
+        }
+
+
+        private void llFfmpeg_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("rundll32", "url.dll,FileProtocolHandler https://github.com/BtbN/FFmpeg-Builds");
+        }
+
+        private void llYdl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("rundll32", "url.dll,FileProtocolHandler https://github.com/ytdl-org/youtube-dl");
+        }
+
+        private async void btnUpdateFfmpeg_Click(object sender, EventArgs e)
+        {
+            btnUpdateFfmpeg.Enabled = false;
+
+            var downloadFileUrl = "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip";
+            var destinationFilePath = Path.GetFullPath("ffmpeg-master-latest-win64-gpl.zip");
+            var destinationFolder = Path.GetDirectoryName(destinationFilePath);
+
+            if (destinationFolder != null && Directory.Exists(destinationFolder))
+            {
+                using (var client = new HttpClientDownloadWithProgress(downloadFileUrl, destinationFilePath))
+                {
+                    client.ProgressChanged += (totalFileSize, totalBytesDownloaded, progressPercentage) =>
+                    {
+                        pbFfmpeg.Value = Convert.ToInt32(progressPercentage ?? 0);
+                        lblStatusFfmpeg.Text = $"Downloading... ({totalBytesDownloaded / 1048576}/{totalFileSize / 1048576}MiB)";
+                    };
+                    await client.StartDownload();
+                }
+
+                pbFfmpeg.Value = 0;
+                lblStatusFfmpeg.Text = $"Extracting...";
+
+                ZipFile.ExtractToDirectory(destinationFilePath, destinationFolder);
+                var SrcFfmpegFilePath = destinationFolder + @"\ffmpeg-master-latest-win64-gpl\bin\ffmpeg.exe";
+                var TarFfmpegFilePath = destinationFolder + @"\ffmpeg.exe";
+                if (File.Exists(SrcFfmpegFilePath))
+                {
+                    File.Copy(SrcFfmpegFilePath, TarFfmpegFilePath);
+
+                    pbFfmpeg.Value = 0;
+                    lblStatusFfmpeg.Text = $"Done [OK]";
+                    lblStatusFfmpeg.ForeColor = Color.Green;
+                }
+                else
+                {
+                    pbFfmpeg.Value = 0;
+                    lblStatusFfmpeg.Text = $"Failed";
+                    lblStatusFfmpeg.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                pbFfmpeg.Value = 0;
+                lblStatusFfmpeg.Text = $"Failed";
+                lblStatusFfmpeg.ForeColor = Color.Red;
+            }
+
+            btnUpdateFfmpeg.Enabled = true;
+        }
+
+        private async void btnUpdateYdl_Click(object sender, EventArgs e)
+        {
+            btnUpdateYdl.Enabled = false;
+
+            var downloadFileUrl = "https://github.com/ytdl-org/youtube-dl/releases/download/2021.12.17/youtube-dl.exe";
+            var destinationFilePath = Path.GetFullPath("youtube-dl.exe");
+            using (var client = new HttpClientDownloadWithProgress(downloadFileUrl, destinationFilePath))
+            {
+                client.ProgressChanged += (totalFileSize, totalBytesDownloaded, progressPercentage) =>
+                {
+                    pbYdl.Value = Convert.ToInt32(progressPercentage ?? 0);
+                    lblStatusYdl.Text = $"Downloading... ({totalBytesDownloaded / 1048576}/{totalFileSize / 1048576}MiB)";
+                };
+                await client.StartDownload();
+            }
+
+            if (File.Exists(destinationFilePath))
+            {
+                pbYdl.Value = 0;
+                lblStatusYdl.Text = $"Done [OK]";
+                lblStatusYdl.ForeColor = Color.Green;
+            }
+            else
+            {
+                pbYdl.Value = 0;
+                lblStatusYdl.Text = $"Failed";
+                lblStatusYdl.ForeColor = Color.Red;
+            }
+
+            btnUpdateYdl.Enabled = true;
         }
     }
 }
